72e4b7df07a879e623ede840d346984cfa698499
Dmytro <dberezhkov2016@gmail.com>
2024-11-10 23:42:05 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Console.WriteLine("It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\nBjarne Stroustrup");
+           
+            //Task 2
+            //int var1 = int.Parse(Console.ReadLine());
+            //int var2 = int.Parse(Console.ReadLine());
+            //int var3 = int.Parse(Console.ReadLine());
+            //int var4 = int.Parse(Console.ReadLine());
+            //int var5 = int.Parse(Console.ReadLine());
+            //int result = var1 + var2 + var3 + var4 + var5;
+            //int max = var1; int min = var1;
+            //if (var2 > max)
+            //{
+            //    max = var2;
+            //}
+            //if (var3 > max)
+            //{
+            //    max = var3;
+            //}
+            //if(var4 > max)
+            //{
+            //    max = var4;
+            //}
+            //if(var5 > max)
+            //{
+            //    max = var5;
+            //}
+
+            //if (var2 < min)
+            //{
+            //    min = var2;
+            //}
+            //if (var3 < min)
+            //{
+            //    min = var3;
+            //}
+            //if (var4 < min)
+            //{
+            //    min = var4;
+            //}
+            //if(var5 < min)
+            //{
+            //    min = var5;
+            //}
+            //Console.WriteLine(result);
+            //Console.WriteLine(max);
+            //Console.WriteLine(min);
+
+
+            //Task 3
+            //int number = int.Parse(Console.ReadLine());
+            //if (number >= 100000 && number <= 999999)
+            //{
+            //    int reverse = 0;
+            //    while(number > 0)
+            //    {
+            //        reverse = reverse * 10 + number % 10;
+            //        number /= 10;
+            //    }
+            //    Console.WriteLine(reverse);
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("You put wrong number. Number must be 6 digit.");
+            //}
+
+            //Task 4
+            //int min = int.Parse(Console.ReadLine());
+            //int max = int.Parse(Console.ReadLine());
+
+            //if (min < max)
+            //{
+            //    int f1 = 0;
+            //    int f2 = 1;
+            //    while (f1 < max)
+            //    {
+            //        if (f1 >= min)
+            //        {
+            //            Console.Write(f1);
+            //            Console.Write(" ");
+            //        }
+
+            //        int next = f1 + f2;
+            //        f1 = f2;
+            //        f2 = next;
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wrong diapason!");
+            //}
+
+            //Task 5
+            //int A = int.Parse(Console.ReadLine());
+            //int B = int.Parse(Console.ReadLine());
+
+
+            //if(A < B)
+            //{
+            //    for(int i = A;  i <= B; i++)
+            //    {
+            //        for(int j = 0; j < i; j++)
+            //        {
+            //            Console.Write(i);
+
+            //        }
+            //        Console.WriteLine(" ");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error!");
+            //}
+
+            //Task 6
+            char symbol = char.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            char go = char.Parse(Console.ReadLine());
+
+            if (go == 'h')
+            {
+                for (int i = 0; i < lenght; i++)
+                {
+                    Console.Write(symbol); 
+                }
+            }
+            else
+            {
+                for(int i = 0;i < lenght; i++)
+                {
+                    Console.WriteLine(symbol);
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //int number = int.Parse(Console.ReadLine());
+            //if (number >= 0 && number <= 100)
+            //{
+            //    if (number % 3 == 0 && number % 5 == 0)
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }
+            //    else if (number % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (number % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Wrong number!");
+            //}
+
+            //Task2
+            //float number = float.Parse(Console.ReadLine());
+            //float percent = float.Parse(Console.ReadLine());
+
+            //float result = (number * percent) / 100;
+            //Console.WriteLine(result);
+
+            //Task3
+            //int num1 = int.Parse(Console.ReadLine());
+            //int num2 = int.Parse(Console.ReadLine());
+            //int num3 = int.Parse(Console.ReadLine());
+            //int num4 = int.Parse(Console.ReadLine());
+            //int result = 0;
+            //int[] arr = new int[4];
+            //arr[0] = num1;
+            //arr[1] = num2;
+            //arr[2] = num3;
+            //arr[3] = num4;
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    result = result * 10 + arr[i];
+            //}
+            //Console.WriteLine(result);
+
+            //Task4
+            //int[] arr = new int[6];
+            //int number = int.Parse(Console.ReadLine());
+            //if (number >= 100000 && number <= 999999)
+            //{
+            //    while (number > 0)
+            //    {
+            //        for (int i = 0; i < arr.Length; i++)
+            //        {
+            //            arr[i] = number % 10;
+            //            number = number / 10;
+            //        }
+            //    }
+            //    Array.Reverse(arr);
+            //    int index1 = int.Parse(Console.ReadLine()) - 1;
+            //    int index2 = int.Parse(Console.ReadLine()) - 1;
+            //    int tmp = arr[index1];
+            //    arr[index1] = arr[index2];
+            //    arr[index2] = tmp;
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+            //        Console.Write(arr[i]);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error!");
+            //}
+
+            //Task6
+            //float temperature = float.Parse(Console.ReadLine());
+            //Console.WriteLine("1 to convert tempereture to Fahrenheit;\n2 to convert temperature to Celsius");
+            //char user_choice = char.Parse(Console.ReadLine());
+            //if (user_choice == '1')
+            //{
+            //    float result = (temperature * 9 / 5) + 32;
+            //    Console.WriteLine(result);
+            //}
+            //else if (user_choice == 2)
+            //{
+            //    float result = (temperature - 32) * 5 / 9;
+            //    Console.WriteLine(result);
+            //}
+
+            //Task7
+            int min = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+            if (min > max)
+            {
+                int tmp = min;
+                min = max;
+                max = tmp;
+                for (int i = min; i <= max; i++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = min; i <= max; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
